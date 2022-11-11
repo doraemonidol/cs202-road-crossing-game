6b36bffb9c3d6e7a4ed780e3d38ec689ebaea884
Dang Hoang Nhat Hung <doraemonidol@gmail.com>
2022-11-12 05:46:57 +0700

    Update main.h

--- cs202-road-crossing-game/cs202-road-crossing-game/main.h
+++ cs202-road-crossing-game/cs202-road-crossing-game/main.h
@@ -10,6 +10,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <string.h>
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Config.hpp>
+#include <SFML/GpuPreference.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 #pragma warning(disable : 4996)
